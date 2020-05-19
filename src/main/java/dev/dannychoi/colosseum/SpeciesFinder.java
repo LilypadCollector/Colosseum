9b38f11161785fd965a186df4f819abd22d080e0
LilypadCollector <dc042501@gmail.com>
2020-05-19 02:17:29 -0700

    Class containing one function that takes in a SpeciesType and returns an instance of the Corresponding Species

---
+++ src/main/java/dev/dannychoi/colosseum/SpeciesFinder.java
@@ -0,0 +1,13 @@
+package dev.dannychoi.colosseum;
+
+// This class is essentially used to get an instance of a certain species.
+public class SpeciesFinder {
+    public static Species getByType(SpeciesType type) {
+        switch (type) {
+            case DOG: return new SpeciesDog();
+        }
+
+        // If we get here, it means I made a new class but forgot to add it to above switch statement.
+        return null;
+    }
+}
